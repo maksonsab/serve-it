521e57671fad12f6cfe5a20b4a43b082f20a44a9
Maksim Sabirzanov <maksonsab@gmail.com>
2022-06-16 14:37:56 +0700

    fixed django settings SECRET_KEY to shell var

--- stat_proj/stat_proj/settings.py
+++ stat_proj/stat_proj/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--f#r821mjl260pm$e-)1+#70s576b&xuc%g8n&*_s2ejumq(d5'
+SECRET_KEY = environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
